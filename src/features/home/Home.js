2e9e984da9d7c045d020f099645ae384d10592dc
Akshaj Verma <akshajverma7@gmail.com>
2020-05-08 11:27:14 +0530

    Added starter frontend to home page.

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,9 +1,20 @@
 import React from "react"
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core"
+import tw from "twin.macro"
 
 const Home = () => {
   return (
-    <section>
-      <h1>Home Page</h1>
+    <section tw="flex flex-1 flex-col h-screen bg-red-500">
+      <section tw="bg-green-500 py-10 px-2">
+        <h3> Hello User!</h3>
+        <p> Your balance is = XXX</p>
+      </section>
+      <section tw="py-2 px-2">
+        <h3> Your Collection </h3>
+        <p> Collection 1 </p>
+        <p> Collection 2 </p>
+      </section>
     </section>
   )
 }
