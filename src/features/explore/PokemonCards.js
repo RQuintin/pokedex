ac8bd73ce5ca65153111becf704d7f945c466736
Akshaj Verma <akshajverma7@gmail.com>
2020-05-09 11:32:04 +0530

    Added basic poke card ui.

--- src/features/explore/PokemonCards.js
+++ src/features/explore/PokemonCards.js
@@ -7,6 +7,28 @@ import React, { useState, useEffect } from "react"
 
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=150"
 
+const PokeCard = () => {
+  return (
+    <div tw="flex flex-row justify-around items-center bg-red-500 p-2">
+      <div tw="">
+        <img
+          tw="bg-cover"
+          alt="pokemon"
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+        />
+      </div>
+      <div tw="">
+        <p>name</p>
+        <p>type</p>
+        <p>stats</p>
+      </div>
+      <div tw="">
+        <p> Button to add to list</p>
+      </div>
+    </div>
+  )
+}
+
 const PokemonCards = () => {
   const [pokeData, setPokeData] = useState([])
 
@@ -19,7 +41,15 @@ const PokemonCards = () => {
     fetchPokemon()
   }, [])
 
-  return <section tw="p-2">Pokemon Cards</section>
+  return (
+    <div tw="p-2">
+      Pokemon Cards
+      <section tw="grid grid-cols-1 gap-2">
+        <PokeCard />
+        <PokeCard />
+      </section>
+    </div>
+  )
 }
 
 export default PokemonCards
