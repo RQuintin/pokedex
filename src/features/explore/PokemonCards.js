143f52a4f819789e929737bbcbc426ed3d1b4103
Akshaj Verma <akshajverma7@gmail.com>
2020-05-09 11:09:33 +0530

    Fetch data.

--- src/features/explore/PokemonCards.js
+++ src/features/explore/PokemonCards.js
@@ -8,6 +8,17 @@ import React, { useState, useEffect } from "react"
 const URL = "https://pokeapi.co/api/v2/pokemon?limit=150"
 
 const PokemonCards = () => {
+  const [pokeData, setPokeData] = useState([])
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const result = await axios.get(URL)
+      console.log(result.data)
+      setPokeData(result.data)
+    }
+    fetchPokemon()
+  }, [])
+
   return <section tw="p-2">Pokemon Cards</section>
 }
 
