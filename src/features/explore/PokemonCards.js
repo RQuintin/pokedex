37ac3ee046df515fd782b0582c2ab496b2501a1c
Akshaj Verma <akshajverma7@gmail.com>
2020-05-09 11:08:00 +0530

    Minor edits.

--- src/features/explore/PokemonCards.js
+++ src/features/explore/PokemonCards.js
@@ -2,7 +2,10 @@
 import { jsx, css } from "@emotion/core"
 import tw from "twin.macro"
 
-import React from "react"
+import axios from "axios"
+import React, { useState, useEffect } from "react"
+
+const URL = "https://pokeapi.co/api/v2/pokemon?limit=150"
 
 const PokemonCards = () => {
   return <section tw="p-2">Pokemon Cards</section>
