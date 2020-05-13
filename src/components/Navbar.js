9a5f211be96aa06679f570d58e5d9bcc592f047b
Akshaj Verma <akshajverma7@gmail.com>
2020-05-13 13:01:46 +0530

    Update home page ui.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,23 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <div>
-      <nav tw="flex flex-row p-2 justify-between text-white bg-purple-700 sm:text-lg ">
+      <nav tw="flex flex-row p-2 md:p-4 justify-between text-gray-200 bg-purple-700 sm:text-lg ">
         <h3> POKEDEX</h3>
         <ul tw="flex flex-row">
-          <li tw="mx-1">
-            <Link to="/"> landing </Link>
+          <li tw="mx-1 md:mx-2">
+            <Link tw="hover:text-white" to="/">
+              landing
+            </Link>
           </li>
-          <li tw="mx-1">
-            <Link to="/home"> home </Link>
+          <li tw="mx-1 md:mx-2">
+            <Link tw="hover:text-white" to="/home">
+              home
+            </Link>
           </li>
-          <li tw="mx-1">
-            <Link to="/explore"> explore </Link>
+          <li tw="mx-1 md:mx-2">
+            <Link tw="hover:text-white" to="/explore">
+              explore
+            </Link>
           </li>
         </ul>
       </nav>
--- src/features/home/UserInfo.js
+++ src/features/home/UserInfo.js
@@ -9,19 +9,19 @@ const UserInfo = () => {
   const currentDate = dateTime.toDateString()
 
   return (
-    <div tw="flex flex-row justify-around items-center bg-blue-800 py-8 px-2">
+    <div tw="flex flex-row justify-around items-center bg-blue-800 py-8 px-2 md:py-10 md:px-4">
       <section tw="w-1/3 text-center bg-gray-200 rounded p-4 mr-1">
-        <h3 tw="text-xl"> Hello User!</h3>
+        <h3 tw="text-xl md:text-2xl lg:text-3xl"> Hello User!</h3>
         <p> {currentDate} </p>
       </section>
-      <section tw="w-2/3 text-left text-white p-4 rounded ml-1">
+      <section tw="w-2/3 text-left text-white p-4 rounded ml-1 sm:text-lg md:text-xl lg:text-2xl md:flex md:flex-row md:justify-center">
         <p tw="m-1 p-2">
           <span tw="bg-yellow-400 text-gray-900 p-2 m-1 rounded">XXX</span>
-          coins left.
+          coins
         </p>
         <p tw="m-1 p-2">
           <span tw="bg-yellow-400 text-gray-900 p-2 m-1 rounded">YYY</span>
-          collections.
+          collections
         </p>
       </section>
     </div>
