53fa12cd8e37dc2d3994a19e9d371bd14a4b8f59
Akshaj Verma <akshajverma7@gmail.com>
2020-05-14 14:55:26 +0530

    Added functionality to search Pokemon.

--- src/features/explore/PokemonCards.js
+++ src/features/explore/PokemonCards.js
@@ -3,14 +3,19 @@ import { jsx, css } from "@emotion/core"
 import tw from "twin.macro"
 import PokemonCard from "./components/PokemonCard"
 
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 import { useDispatch, useSelector } from "react-redux"
-import { fetchPokemonNameUrl, selectorPokemon } from "./pokemonCardsSlice"
+import {
+  fetchPokemonNameUrl,
+  selectorFilteredPokemon,
+  selectorPokemon,
+} from "./pokemonCardsSlice"
 
 const PokemonCards = () => {
   const dispatch = useDispatch()
   const pokemonList = useSelector(selectorPokemon)
+  const filteredPokemonList = useSelector(selectorFilteredPokemon)
 
   useEffect(() => {
     dispatch(fetchPokemonNameUrl())
@@ -20,18 +25,31 @@ const PokemonCards = () => {
     <div tw="p-2 md:p-4">
       <ul>
         <div tw="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-3 lg:gap-4">
-          {pokemonList.map(poke => (
-            <PokemonCard
-              key={`key-${poke.id}`}
-              pokemonId={poke.id}
-              pokemonName={poke.name}
-              pokemonType={poke.type}
-              pokemonHeight={poke.height}
-              pokemonWeight={poke.weight}
-              pokemonBaseExperience={poke.baseExperience}
-              pokemonSprite={poke.sprites}
-            />
-          ))}
+          {filteredPokemonList.length === 0
+            ? pokemonList.map(poke => (
+                <PokemonCard
+                  key={`key-${poke.id}`}
+                  pokemonId={poke.id}
+                  pokemonName={poke.name}
+                  pokemonType={poke.type}
+                  pokemonHeight={poke.height}
+                  pokemonWeight={poke.weight}
+                  pokemonBaseExperience={poke.baseExperience}
+                  pokemonSprite={poke.sprites}
+                />
+              ))
+            : filteredPokemonList.map(poke => (
+                <PokemonCard
+                  key={`key-${poke.id}`}
+                  pokemonId={poke.id}
+                  pokemonName={poke.name}
+                  pokemonType={poke.type}
+                  pokemonHeight={poke.height}
+                  pokemonWeight={poke.weight}
+                  pokemonBaseExperience={poke.baseExperience}
+                  pokemonSprite={poke.sprites}
+                />
+              ))}
         </div>
       </ul>
     </div>
--- src/features/explore/SearchPokemon.js
+++ src/features/explore/SearchPokemon.js
@@ -2,21 +2,37 @@
 import { jsx, css } from "@emotion/core"
 import tw from "twin.macro"
 
-import React from "react"
+import React, { useState } from "react"
+
+import { useDispatch } from "react-redux"
+import { search } from "./pokemonCardsSlice"
 
 const SearchPokemon = () => {
+  const dispatch = useDispatch()
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+  const handleSearchTermChange = e => {
+    setSearchTerm(e.target.value)
+    dispatch(search({ searchTerm: e.target.value }))
+  }
+
   return (
     <div tw="flex flex-row bg-blue-800 py-8 px-2 md:py-10 md:px-4">
       <section tw="text-lg md:text-xl">
         <h3 tw="text-white"> Search Pokemon </h3>
         <form>
-          <input tw="rounded" />
-          <button
+          <input
+            tw="rounded"
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
+          {/* <button
             tw="bg-purple-700 hover:bg-purple-800 text-gray-200 hover:text-white rounded p-2 m-1"
             type="submit"
           >
             search
-          </button>
+          </button> */}
         </form>
       </section>
     </div>
