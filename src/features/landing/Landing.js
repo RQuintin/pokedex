16f1ea4018434453e8eca6ffda5dd31bb46f5d64
Akshaj Verma <akshajverma7@gmail.com>
2020-05-18 14:45:27 +0530

    Added signup and login formss.

--- src/features/landing/Landing.js
+++ src/features/landing/Landing.js
@@ -11,11 +11,28 @@ const ModalLogin = props => {
   const { loginModalBool, setLoginModalBool } = props
 
   return (
-    <div tw="bg-gray-200 text-gray-900 rounded-lg shadow-lg p-20 md:p-32 lg:p-48">
-      <div> Login Modal open. </div>
-      <button onClick={() => setLoginModalBool(!loginModalBool)}>
-        Close modal.
-      </button>
+    <div tw="flex flex-col text-center h-full w-64 bg-gray-200 text-gray-900 rounded-lg shadow-lg p-2 md:p-4 lg:p-6">
+      <div tw="flex flex-row justify-between">
+        <p tw="text-lg">Login</p>
+        <button tw="text-sm" onClick={() => setLoginModalBool(!loginModalBool)}>
+          close
+        </button>
+      </div>
+      <div tw="flex flex-col justify-around my-1">
+        <form tw="">
+          <p tw="my-1">
+            username
+            <input />
+          </p>
+          <p tw="my-1">
+            password
+            <input />
+          </p>
+          <button tw="my-1 p-1 rounded bg-gray-800 text-gray-100 hover:bg-gray-900">
+            log in
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
@@ -24,11 +41,31 @@ const ModalSignup = props => {
   const { signupModalBool, setSignupModalBool } = props
 
   return (
-    <div tw="bg-gray-200 text-gray-900 rounded-lg shadow-lg p-24 md:p-32 lg:p-48">
-      <div> Signup Modal open. </div>
-      <button onClick={() => setSignupModalBool(!signupModalBool)}>
-        Close modal.
-      </button>
+    <div tw="flex flex-col text-center h-full w-64 bg-gray-200 text-gray-900 rounded-lg shadow-lg p-2 md:p-4 lg:p-6">
+      <div tw="flex flex-row justify-between">
+        <p tw="text-lg">Sign up</p>
+        <button
+          tw="text-sm"
+          onClick={() => setSignupModalBool(!signupModalBool)}
+        >
+          close
+        </button>
+      </div>
+      <div tw="flex flex-col justify-around my-1">
+        <form tw="">
+          <p tw="my-1">
+            username
+            <input />
+          </p>
+          <p tw="my-1">
+            password
+            <input />
+          </p>
+          <button tw="my-1 p-1 rounded bg-gray-800 text-gray-100 hover:bg-gray-900">
+            sign up
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
@@ -74,7 +111,7 @@ const Landing = () => {
         ) : (
           <div>
             <ButtonBase onClick={() => setLoginModalBool(!loginModalBool)}>
-              login
+              log in
             </ButtonBase>
             <ButtonBase onClick={() => setSignupModalBool(!signupModalBool)}>
               sign up
